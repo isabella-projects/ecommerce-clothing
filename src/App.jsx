1f8ff966a79ef8435259771937b161766d79bd47
isabella-projects <damyan4oy@gmail.com>
2023-11-20 17:49:29 +0100

    Preparation for the project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+    return (
+        <div>
+            Hello World!
+            <p>I&apos;m a paragraph</p>
+        </div>
+    );
+}
+
+export default App;
