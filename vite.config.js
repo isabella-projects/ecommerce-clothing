b4ecdcdda7ba7c07de861ef62ec6ffef5398a05b
isabella-projects <damyan4oy@gmail.com>
2023-11-22 16:39:11 +0100

    Instant server open script

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    server: {
+        open: "ecommerce-clothing",
+    },
     base: "/ecommerce-clothing",
     plugins: [react()],
 });
