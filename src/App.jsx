f920139cbe24d6f83b38714330a041be91222284
isabella-projects <damyan4oy@gmail.com>
2023-11-22 17:54:25 +0100

    Basic view and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,42 @@
-import "./App.css";
+import "./categories.styles.scss";
 
-function App() {
+const App = () => {
+    const categories = [
+        {
+            id: 1,
+            title: "Hats",
+        },
+        {
+            id: 2,
+            title: "Jackets",
+        },
+        {
+            id: 3,
+            title: "Sneakers",
+        },
+        {
+            id: 4,
+            title: "Womens",
+        },
+        {
+            id: 5,
+            title: "Mans",
+        },
+    ];
+
     return (
-        <div>
-            Hello World!
-            <p>I&apos;m a paragraph</p>
+        <div className="categories-container">
+            {categories.map(({ title, id }) => (
+                <div key={id} className="category-container">
+                    <div className="background-image"></div>
+                    <div className="category-body-container">
+                        <h2>{title}</h2>
+                        <p>Shop Now</p>
+                    </div>
+                </div>
+            ))}
         </div>
     );
-}
+};
 
 export default App;
