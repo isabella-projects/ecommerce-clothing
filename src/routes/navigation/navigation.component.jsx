dc4451625ffda7cb795a072b8402ace462735007
isabella-projects <damyan4oy@gmail.com>
2023-11-29 03:29:58 +0100

    Some small styling in order to stay up to date with project

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,15 +1,16 @@
 import { Outlet, Link } from "react-router-dom";
 
-import CrwnLogo from "../../assets/img/crown.svg?react";
+// import CrwnLogo from "../../assets/img/crown.svg?react";
 
 import "./navigation.styles.scss";
+import firmLogo from "../../assets/img/perfect-color.jpg";
 
 const Navigation = () => {
     return (
         <>
             <div className="navigation">
                 <Link className="logo-container" to="/">
-                    <CrwnLogo className="logo" />
+                    <img className="logo" src={firmLogo} alt="Firm Logo" />
                 </Link>
                 <div className="nav-links-container">
                     <Link className="nav-link" to="/shop">
