6f2ee6ec3523a40c31bd806c5f6fd8bd17d5f9ab
isabella-projects <damyan4oy@gmail.com>
2024-04-14 14:24:58 +0200

    Added product context - preparation for storing in firestore db

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 
 import { UserProvider } from './contexts/user.context.jsx';
+import { ProductsProvider } from './contexts/products.context.jsx';
 
 import './index.scss';
 
@@ -12,7 +13,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <BrowserRouter>
             <UserProvider>
-                <App />
+                <ProductsProvider>
+                    <App />
+                </ProductsProvider>
             </UserProvider>
         </BrowserRouter>
     </React.StrictMode>
