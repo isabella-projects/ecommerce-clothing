805d3adb456cd6bf7ccef45ca2b5d7fa9ae715f7
isabella-projects <damyan4oy@gmail.com>
2024-08-06 17:14:10 +0200

    Fixed a warning message for selector with Redux Toolkit

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,10 +1,17 @@
-export const selectCategoriesMap = (state) => {
-    console.log('Selector fired!');
+import { createSelector } from '@reduxjs/toolkit';
 
-    return state.categories.categories.reduce((accumulator, category) => {
+const selectCategoryReducer = (state) => state.categories;
+
+export const selectCategories = createSelector(
+    [selectCategoryReducer],
+    (categoriesSlice) => categoriesSlice.categories
+);
+
+export const selectCategoriesMap = createSelector([selectCategories], (categories) =>
+    categories.reduce((accumulator, category) => {
         const { title, items } = category;
         accumulator[title.toLowerCase()] = items;
 
         return accumulator;
-    }, {});
-};
+    }, {})
+);
